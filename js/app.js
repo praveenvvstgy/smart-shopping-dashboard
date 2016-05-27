11b20c38300d5088296dbc4f3cd0d43956b7694c
Gowda I V, Praveen <praveen.gowda.i.v@sap.com>
2016-05-27 08:56:34 +0530

    Some Commit

--- js/app.js
+++ js/app.js
@@ -12,34 +12,37 @@ var Firebase = {
 		this.database = this.firebase.database();
 		this.cartId = "cart1";
 		this.users = {"default": {"name": "...", "age": "...", "address": "...", "lastPurchase": "..."}};
+		this.table = jQuery("#cartContents");
 		var that = this
 		this.database.ref('/users').once('value').then(function(snapshot) {
+			console.log("All users Loaded")
 			that.users = jQuery.extend(that.users, snapshot.val())
-		})
-
-		this.bindActiveUser()
-
-		// Watch for changes to default user
-		this.database.ref('carts/' + this.cartId + '/activeUser').on('child_added', function(snapshot) {
-			console.log("Current User is " + snapshot.key)
-			that.currentUser.name = that.users[snapshot.key].name
-			that.currentUser.age = that.users[snapshot.key].age
-			that.currentUser.address = that.users[snapshot.key].address
-			if (snapshot.key !== "default") {
-				var purchases = that.users[snapshot.key].purchases
-				var purchaseKeys = Object.keys(purchases).sort()
-				if (purchaseKeys.length == 0) {
-					that.currentUser.lastPurchase = "New Customer"
+			that.bindActiveUser()
+			// Watch for changes to default user
+			that.database.ref('carts/' + that.cartId + '/activeUser').on('child_added', function(snapshot) {
+				console.log("Current User is " + snapshot.key)
+				that.currentUser.name = that.users[snapshot.key].name
+				that.currentUser.age = that.users[snapshot.key].age
+				that.currentUser.address = that.users[snapshot.key].address
+				if (snapshot.key !== "default") {
+					var purchases = that.users[snapshot.key].purchases
+					var purchaseKeys = Object.keys(purchases).sort()
+					if (purchaseKeys.length == 0) {
+						that.currentUser.lastPurchase = "New Customer"
+					} else {
+						that.currentUser.lastPurchase = purchases[purchaseKeys.sort().pop()].date
+					}
 				} else {
-					that.currentUser.lastPurchase = purchases[purchaseKeys.sort().pop()].date
+					that.currentUser.lastPurchase = "..."
+					that.refreshUsers()
 				}
-			} else {
-				that.currentUser.lastPurchase = "..."
-			}
-		})
+			});
+		});
+		this.loadMovies();
 	},
 
 	bindActiveUser: function() {
+		console.log("Binding User to DOM")
 		this.currentUser = Bind({
 			name: "...",
 			age: "...",
@@ -51,6 +54,55 @@ var Firebase = {
 			address: '#caddr',
 			lastPurchase: '#cdate'
 		})
+	},
+
+	refreshUsers: function() {
+		var that = this
+		this.database.ref('/users').once('value').then(function(snapshot) {
+			console.log("Refreshed users")
+			that.users = jQuery.extend(that.users, snapshot.val())
+		});
+	}, 
+
+	addMovieToTable: function(tokenId) {
+		var movieId = this.tokenToMovie[tokenId]
+		var movie = this.getMovieById(movieId)
+		this.table.append("<tr id = \"" +  tokenId + "\"><td>" + movie.movieId + "</td><td>" +  movie.title + "</td><td> ₹<span class=\"priceCell\">" +  movie.price + "</span></td></tr>")
+		this.updateTableTotal();
+	},
+
+	removeMovieFromTable: function(tokenId) {
+		jQuery("#" + tokenId).remove();
+		this.updateTableTotal();
+	},
+
+	updateTableTotal: function() {
+		var sum = 0;
+		$(".priceCell").each(function() {
+			var price = $(this)
+			sum += parseInt(price.html())
+		})
+		$("#totalCell").html(sum);
+	},
+
+	loadMovies: function() {
+		var that = this
+		jQuery.getJSON("data/movies_processed.json", function(json) {
+			that.movies = json
+			that.database.ref('/tokenToMovie').once('value').then(function(snapshot) {
+				that.tokenToMovie = snapshot.val();
+				that.database.ref('carts/' + that.cartId + '/contents').on('child_added', function(snapshot) {
+					that.addMovieToTable(snapshot.key)
+				});
+				that.database.ref('carts/' + that.cartId + '/contents').on('child_removed', function(snapshot) {
+					that.removeMovieFromTable(snapshot.key)
+				});
+			});
+		});
+	},
+
+	getMovieById: function(id) {
+		return this.movies[id - 1];
 	}
 }
 
